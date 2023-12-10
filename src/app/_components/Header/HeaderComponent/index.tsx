85ff769d13d54bb8871781210baf3903fcba5e0d
elmehdibenhayoun <elmehdi.benhayoun@gmail.com>
2023-12-10 02:29:24 +0100

    second commit

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+
+import classes from './index.module.scss'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  return (
+    <nav className={classes.header}>
+      <Gutter className={classes.wrap}>
+        <Link href="/">
+          <Image src="/logo-black.svg" alt="logo" height={50} width={170} />
+        </Link>
+      </Gutter>
+    </nav>
+  )
+}
+
+export default HeaderComponent
